d4aad36a9d51e91e960cd25161b52fc65ea9ca26
Hung Vo Viet <vohungbk@gmail.com>
2024-12-18 01:11:07 +0700

    Add authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { AuthProvider } from '@/providers/AuthProvider';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Code Horizons: Navigating the World of Development",
-  description: "Insights, tips, and resources for developers striving to master web development, programming languages, and cutting-edge tech tools.",
+  title: 'Code Horizons: Navigating the World of Development',
+  description:
+    'Insights, tips, and resources for developers striving to master web development, programming languages, and cutting-edge tech tools.',
 };
 
 export default function RootLayout({
@@ -23,8 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.className} ${geistMono.className} antialiased`}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={`${geistSans.className} ${geistMono.className} antialiased`}>
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <h4>Hello</h4>;
+import { Button } from '@/components/ui/button';
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
+
+  return (
+    <div className="p-10">
+      <h4>Hello</h4>
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ) : (
+        <>
+          <RegisterLink>
+            <Button>Register</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Login</Button>
+          </LoginLink>
+        </>
+      )}
+    </div>
+  );
 }
