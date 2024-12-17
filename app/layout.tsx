1989c9b4180b41f3e0eb385e1a14bf2623c7dafb
Hung Vo Viet <vohungbk@gmail.com>
2024-12-18 00:32:02 +0700

    Add basic config

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Code Horizons: Navigating the World of Development",
+  description: "Insights, tips, and resources for developers striving to master web development, programming languages, and cutting-edge tech tools.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.className} ${geistMono.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
